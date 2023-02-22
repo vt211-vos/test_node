1bd247d904e5e3118ed404c6e013a79e8e6d5adb
Vygnich Oleksandr <vt211_vos@student.ztu.edu.ua>
2023-02-22 18:36:48 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,80 @@
+import express from "express"
+import cors from 'cors'
+
+const app = express();
+app.use(cors({
+    origin: '*'}));
+
+const data = [
+    {
+        'id': "1",
+        "questions": [
+            {
+                id: "1",
+                name: "Rate something",
+                score: 3
+            },
+            {
+                id: "2",
+                name: "Rate something",
+                score: 4
+            },
+            {
+                id: "3",
+                name: "Rate something",
+                score: 1
+            },
+        ]
+    },
+    {
+        'id': "2",
+        "questions": [
+            {
+                id: "1",
+                name: "Rate something",
+                score: 3
+            },
+            {
+                id: "5",
+                name: "Rate something",
+                score: 4
+            },
+            {
+                id: "6",
+                name: "Rate something",
+                score: 1
+            },
+        ]
+    },
+    {
+        'id': "3",
+        "questions": [
+            {
+                id: "7",
+                name: "Rate some thing",
+                score: 3
+            },
+            {
+                id: "8",
+                name: "Rate some thing",
+                score: 4
+            },
+            {
+                id: "9",
+                name: "Rate some thing",
+                score: 1
+            },
+        ]
+    },
+    ]
+
+app.get('/', (req, res)=>{
+    res.send(data)
+})
+
+app.listen(4444, (err)=>{
+    if(err){
+        return console.log(err)
+    }
+    console.log("Server OK")
+})
